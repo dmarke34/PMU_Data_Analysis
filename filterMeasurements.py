5599e128fe81612e81aca2ced10b1e1eab1e9be7
dmarke34 <ajaparicio.cpp@edu>
2022-07-16 11:33:48 -0700

    filtered original data

--- filterMeasurements.py
+++ filterMeasurements.py
@@ -21,10 +21,19 @@ rocof = measurements[:,7]
 
 volMag[volMag=='']=0.0
 volAng[volAng=='']=0.0
+curMag[curMag=='']=0.0
+curAng[curAng=='']=0.0
+freq[freq=='']=0.0
+rocof[rocof=='']=0.0
 
-xfloat = volMag.astype(float)
-yfloat = volAng.astype(float)
+volMag = volMag.astype(float)
+volAng = volAng.astype(float)
+curMag = curMag.astype(float)
+curAng = curAng.astype(float)
+volAng = volAng.astype(float)
+freq = freq.astype(float)
+rocof = rocof.astype(float)
 
-combined = np.stack((xfloat, yfloat)).T
+combined = np.stack((pckNum,volMag, volAng, curMag, curAng, volAng, freq, rocof)).T
 
-print(combined)
+pd.DataFrame(combined, columns='Packet').to_csv("filteredData.csv")
