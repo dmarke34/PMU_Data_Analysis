13c7ff77281718abeb7ea4712f4a7a679b12592d
dmarke34 <ajaparicio.cpp@edu>
2022-07-16 12:05:22 -0700

    removed all zero measurements

--- filterMeasurements.py
+++ filterMeasurements.py
@@ -7,6 +7,7 @@ from numpy import dtype, float64
 with open("pmuData.csv", 'r') as i:
     rawdata = list(csv.reader(i, delimiter= ','))
 
+#read measurement arrays and set them in individual arrays
 measurements = np.array(rawdata[1:], dtype=str)
 pckNum = measurements[:,0]
 
@@ -19,6 +20,7 @@ curAng = measurements[:,5]
 freq = measurements[:,6]
 rocof = measurements[:,7]
 
+#add '0' as element to empty indexes
 volMag[volMag=='']=0.0
 volAng[volAng=='']=0.0
 curMag[curMag=='']=0.0
@@ -26,14 +28,22 @@ curAng[curAng=='']=0.0
 freq[freq=='']=0.0
 rocof[rocof=='']=0.0
 
+#string to float
+pckNum = pckNum.astype(int)
 volMag = volMag.astype(float)
 volAng = volAng.astype(float)
 curMag = curMag.astype(float)
 curAng = curAng.astype(float)
-volAng = volAng.astype(float)
 freq = freq.astype(float)
 rocof = rocof.astype(float)
 
-combined = np.stack((pckNum,volMag, volAng, curMag, curAng, volAng, freq, rocof)).T
+combined = np.stack((pckNum,volMag, volAng, curMag, curAng, freq, rocof)).T
+counter = 0
+for x in combined:
+    if combined[counter][1] == 0.0:
+        combined = np.delete(combined, counter, 0)
+        counter -= 1
+    counter += 1
 
-pd.DataFrame(combined, columns='Packet').to_csv("filteredData.csv")
+pd.DataFrame(combined, columns=['Packet' ,'Voltage Mag', 'Voltage Angle', 'Current Mag',
+'Current Angle', 'Actual Frequency', 'ROCOF']).to_csv("filteredData.csv")
